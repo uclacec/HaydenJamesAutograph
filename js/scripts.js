c3dd1ff146759813bbe8e7d21de092e93ecd4e8d
Daniel Huang <danielhuang@g.ucla.edu>
2016-05-20 17:46:23 -0700

    merged files

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Daniel on 5/20/2016.
+ */
+(function(){
+    //when clicked
+    $('#btn').on('click',introAnimate);
+
+    function introAnimate(){
+        var autograf=$('#autograf');
+        var haydenjames=$('#haydenjames');
+        var line=$('#line');
+        //animates autograf logo to bottom left
+        autograf.animate({
+            top:62+'vh',
+            left:1+'vw',
+            width:10+'vw'
+        },1000);
+        autograf.addClass('col-md-1', 1000);
+
+        //animates haydenjames logo to top right
+        haydenjames.animate({
+            top:-13+'vh',
+            left:78+'vw',
+            width:10+'vw'
+        },1000);
+        haydenjames.addClass('col-md-1',1000);
+
+        //animates line to take up more width
+        line.animate({
+            width:60+'vw',
+            left:20+'vw',
+            opacity:.8
+        },1000);
+        line.addClass('col-md-8',1000);
+        setTimeout(function(){
+            $('#btn').fadeOut();
+        },500)
+    }
+})();
