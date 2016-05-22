e93351192109cc762eeab391373065bdff64592d
Daniel Huang <danielhuang@g.ucla.edu>
2016-05-22 00:00:10 -0700

    1. finished random quote generator 2. takes correct screenshot of post 3. saves post onto firebase 4. displays post image onto page 5. need to get firebase url to post to facebook

---
+++ js/app.js
@@ -0,0 +1,66 @@
+/**
+ * Created by Daniel on 5/21/2016.
+ */
+(function(){
+    var list=[
+        {
+            fl:"There's something about you",
+            ll:" There's something about",
+            sign:"."
+        },
+        {
+            fl:"What about a hit?",
+            ll:"What about a hit of your",
+            sign:"."
+        },
+        {
+          fl:"Start to shake",
+            ll:"Start to shake with your",
+            sign:"."
+        },
+        {
+            fl:"What of a click?",
+            ll: "What of a click, are you a",
+            sign:"?"
+        },
+        {
+            fl:"You turn and face me",
+            ll:"Maybe this time I'll",
+            sign:"."
+        },
+        {
+            fl:"When are you coming back",
+            ll:"I miss your strong",
+            sign:"."
+        },
+        {
+            fl:"I want to believe we have",
+            ll:"Chance to let our hearts",
+            sign:"."
+        },
+        {
+            fl:"I need you here with me",
+            ll:"You see it on my",
+            sign:"."
+        },
+        {
+            fl:"And know that when our heart beats run",
+            ll:"Troubles will be",
+            sign:"."
+        },
+        {
+            fl:"I get permission to love",
+            ll:"I get permission to",
+            sign:"."
+        },
+        {
+            fl:"I get the mission to love",
+            ll:"I get the mission to",
+            sign:"."
+        }
+    ];
+    var randLyric=Math.floor((Math.random() * 11 ) + 1)-1;
+    $('#first-line').text(list[randLyric].fl);
+    $('#last-line').text(list[randLyric].ll);
+    $('#sign').text(list[randLyric].sign);
+})();
--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,22 @@
  * Created by Daniel on 5/20/2016.
  */
 (function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDjsSaVcA5Sb_JB5QujIK3Ayhwao2Knlgc",
+        authDomain: "project-8001030731840866776.firebaseapp.com",
+        databaseURL: "https://project-8001030731840866776.firebaseio.com",
+        storageBucket: "",
+    };
+    firebase.initializeApp(config);
+    // Get a reference to the database service
+    var database = firebase.database();
+
     //when clicked
     $('#btn').on('click',introAnimate);
-
+    //var original= firebase.database().ref('imageName');
+    //var imgname= firebase.database().update('imageName');
+    var num;
     function introAnimate(){
         var autograf=$('#autograf');
         var haydenjames=$('#haydenjames');
@@ -31,11 +44,38 @@
         },1000);
         setTimeout(function(){
             $('#line-content').fadeIn();
-        },1000);
+        },1500);
 
 
         setTimeout(function(){
             $('#btn').fadeOut();
-        },500)
+        },500);
+        var post=document.getElementById("autograf");
+        $('#postinput').on("click",function(){
+            html2canvas(document.getElementById("post")).then(function(canvas) {
+                //document.body.appendChild(canvas);
+                var a = document.createElement('a');
+                //saves canvas image as jpg
+                a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+                num++;
+                //imgname.update(num);
+
+                //push url to firebase
+                database.ref('images').push(a.href);
+
+                var imgRef = firebase.database().ref('images/');
+                imgRef.on('child_added', function(data) {
+                    $("#realpost").attr("src",data.val());
+                    //fbs_click(post);
+                    window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(imgRef+data.val)+'&t=','sharer','toolbar=0,status=0,width=626,height=436');
+                });
+            });
+        });
+        function fbs_click(TheImg) {
+            u=TheImg.src;
+            // t=document.title;
+            t=TheImg.getAttribute('alt');
+            window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;
+        }
     }
 })();
